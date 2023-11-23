50821e6ccb28f7a6878640874b9aba387767220c
Jack <75576058+jackholden@users.noreply.github.com>
2023-11-23 15:17:20 +0000

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        splashscreen.close().unwrap();
+    }
+    window.get_window("main").unwrap().show().unwrap();
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust yes!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet, close_splashscreen])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
